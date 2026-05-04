9b39e04f490b4ff6789020384892bd7a2212a87d
Kenyada Peoples <beautyuniversity1986@gmail.com>
2026-05-04 04:12:21 -0500

    Update ProductCard.jsx
    This file creates a reusable product card that shows product details like the image, title, price, and brand. It also lets users click on a product to see more detailed information.

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,3 +1,14 @@
-export default function ProductCard() {
-  return null;
-}
+export default function ProductCard({ product, onSelect, getStockStatus }) {
+    return (
+      <div className="card" onClick={() => onSelect(product)}>
+        <img src={product.thumbnail} alt={product.title} />
+  
+        <h3>{product.title}</h3>
+        <p>{product.category}</p>
+        <p>${product.price}</p>
+        <p>⭐ {product.rating}</p>
+        <p>Brand: {product.brand}</p>
+        <p>Stock Status: {getStockStatus(product.stock)}</p>
+      </div>
+    );
+  }
